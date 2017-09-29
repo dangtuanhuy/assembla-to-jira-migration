d534250228a43b27fec88c0672046a3ad100c523
Kiffin Gish <kiffin.gish@planet.nl>
2017-09-29 16:31:21 +0200

    Conversions assembla links to jira links for tickets/issues and comments.

--- 99-jira_scan_tickets.rb
+++ 99-jira_scan_tickets.rb
@@ -111,86 +111,90 @@ def collect_list(type, item)
   assembla_ticket_nr = type == 'ticket' ? item['assembla_ticket_number'] : @ticket_a_id_to_a_nr[item['assembla_ticket_id']]
   jira_ticket_key = item['jira_ticket_key']
 
-  if assembla_ticket_nr == '620'
-    puts
-  end
-
   # Split content into lines, and ignore the first line.
   lines = split_into_lines(content)
   lines.shift
   lines.each do |line|
-    next unless line.strip.length.positive? && (@re_comment.match(line) || @re_ticket.match(line))
-    space = $1
+    next unless line.strip.length.positive?
+    line_after = line
+                 .gsub(@re_comment) { |match| "COMMENT[space='#{$1}', link='#{$2}', comment='#{$3}']"}
+                 .gsub(@re_ticket) { |match| "TICKET[space='#{$1}', link='#{$2}']"}
 
-    assembla_link_ticket_nr = $2
-    # jira_link_ticket_key => calculated from link_ticket_a_nr_to_j_key(assembla_link_ticket_nr)
-
-    assembla_link_comment_id = $3
-    # jira_link_comment_id => calculated from link_comment_a_id_to_j_id(assembla_link_comment_id)
-
-    match = $&
-    replace_with = ''
-
-    # IMPORTANT: Following must come after previous three statements (to preserve values $2, $3 and $&)
-    project = @project_by_space[space]
-
-    @spaces[space] = 0 unless @spaces[space]
-    @spaces[space] += 1
-
-    is_link_comment = !assembla_link_comment_id.nil?
-
-    assembla_comment_id = ''
-    assembla_comment_id = type == 'comment' ? item['assembla_comment_id'] : ''
-    jira_comment_id = type == 'comment' ? item['jira_comment_id'] : ''
-    jira_link_ticket_key = ''
-    assembla_link_comment_id ||= ''
-    jira_link_comment_id = ''
-
-    # TICKET:  jira_ticket_id,jira_ticket_key,assembla_ticket_id,assembla_ticket_number
-    # COMMENT: jira_comment_id,jira_ticket_id,jira_ticket_key,assembla_comment_id,assembla_ticket_id
-
-    # type                         TICKET                 COMMENT
-    #                              ======                 =======
-    # is_link_comment(True/False)  F(ticket)  T(comment)  F(ticket) T(comment)
-    #                              ---------  ----------  --------- ----------
-    # assembla_ticket_nr           x          x           x         x
-    # jira_ticket_key              x          x           x         x
-    # assembla_link_ticket_nr      x          x           x         x
-    # jira_link_ticket_key         x          x           x         x (calculated)
-    #
-    # assembla_comment_id          o          o           x         x
-    # jira_comment_id              o          o           x         x
-    # assembla_link_comment_id     o          x           o         x
-    # jira_link_comment_id         o          x           o         x (calculated)
-
-    if project
-      jira_link_ticket_key = link_ticket_a_nr_to_j_key(space, assembla_link_ticket_nr)
-      if is_link_comment
-        jira_link_comment_id = link_comment_a_id_to_j_id(space, assembla_link_comment_id)
-        replace_with = "#{JIRA_API_BASE}/#{JIRA_API_BROWSE_COMMENT.sub('[:jira-ticket-key]', jira_link_ticket_key).sub('[:jira-comment-id]', jira_link_comment_id)}"
-      else
-        replace_with = "#{JIRA_API_BASE}/#{JIRA_API_BROWSE_ISSUE.sub('[:jira-ticket-key]', jira_link_ticket_key)}"
-      end
+    if line_after != line
+      puts "BEFORE: '#{line}'"
+      puts "AFTER : '#{line_after}'"
     end
-
-    @list << {
-      result: project ? 'OK' : 'SKIP',
-      replace: replace_with == '' ? 'NO' : 'YES',
-      space: space,
-      type: type,
-      is_link_comment: is_link_comment,
-      assembla_ticket_nr: assembla_ticket_nr,
-      jira_ticket_key: jira_ticket_key,
-      assembla_comment_id: assembla_comment_id,
-      jira_comment_id: jira_comment_id,
-      assembla_link_ticket_nr: assembla_link_ticket_nr,
-      jira_link_ticket_key: jira_link_ticket_key,
-      assembla_link_comment_id: assembla_link_comment_id,
-      jira_link_comment_id: jira_link_comment_id,
-      match: match,
-      replace_with: replace_with,
-      line: line
-    }
+    # space = $1
+    #
+    # assembla_link_ticket_nr = $2
+    # # jira_link_ticket_key => calculated from link_ticket_a_nr_to_j_key(assembla_link_ticket_nr)
+    #
+    # assembla_link_comment_id = $3
+    # # jira_link_comment_id => calculated from link_comment_a_id_to_j_id(assembla_link_comment_id)
+    #
+    # match = $&
+    # replace_with = ''
+    #
+    # # IMPORTANT: Following must come after previous three statements (to preserve values $2, $3 and $&)
+    # project = @project_by_space[space]
+    #
+    # @spaces[space] = 0 unless @spaces[space]
+    # @spaces[space] += 1
+    #
+    # is_link_comment = !assembla_link_comment_id.nil?
+    #
+    # assembla_comment_id = ''
+    # assembla_comment_id = type == 'comment' ? item['assembla_comment_id'] : ''
+    # jira_comment_id = type == 'comment' ? item['jira_comment_id'] : ''
+    # jira_link_ticket_key = ''
+    # assembla_link_comment_id ||= ''
+    # jira_link_comment_id = ''
+    #
+    # # TICKET:  jira_ticket_id,jira_ticket_key,assembla_ticket_id,assembla_ticket_number
+    # # COMMENT: jira_comment_id,jira_ticket_id,jira_ticket_key,assembla_comment_id,assembla_ticket_id
+    #
+    # # type                         TICKET                 COMMENT
+    # #                              ======                 =======
+    # # is_link_comment(True/False)  F(ticket)  T(comment)  F(ticket) T(comment)
+    # #                              ---------  ----------  --------- ----------
+    # # assembla_ticket_nr           x          x           x         x
+    # # jira_ticket_key              x          x           x         x
+    # # assembla_link_ticket_nr      x          x           x         x
+    # # jira_link_ticket_key         x          x           x         x (calculated)
+    # #
+    # # assembla_comment_id          o          o           x         x
+    # # jira_comment_id              o          o           x         x
+    # # assembla_link_comment_id     o          x           o         x
+    # # jira_link_comment_id         o          x           o         x (calculated)
+    #
+    # if project
+    #   jira_link_ticket_key = link_ticket_a_nr_to_j_key(space, assembla_link_ticket_nr)
+    #   if is_link_comment
+    #     jira_link_comment_id = link_comment_a_id_to_j_id(space, assembla_link_comment_id)
+    #     replace_with = "#{JIRA_API_BASE}/#{JIRA_API_BROWSE_COMMENT.sub('[:jira-ticket-key]', jira_link_ticket_key).sub('[:jira-comment-id]', jira_link_comment_id)}"
+    #   else
+    #     replace_with = "#{JIRA_API_BASE}/#{JIRA_API_BROWSE_ISSUE.sub('[:jira-ticket-key]', jira_link_ticket_key)}"
+    #   end
+    # end
+    #
+    # @list << {
+    #   result: project ? 'OK' : 'SKIP',
+    #   replace: replace_with == '' ? 'NO' : 'YES',
+    #   space: space,
+    #   type: type,
+    #   is_link_comment: is_link_comment,
+    #   assembla_ticket_nr: assembla_ticket_nr,
+    #   jira_ticket_key: jira_ticket_key,
+    #   assembla_comment_id: assembla_comment_id,
+    #   jira_comment_id: jira_comment_id,
+    #   assembla_link_ticket_nr: assembla_link_ticket_nr,
+    #   jira_link_ticket_key: jira_link_ticket_key,
+    #   assembla_link_comment_id: assembla_link_comment_id,
+    #   jira_link_comment_id: jira_link_comment_id,
+    #   match: match,
+    #   replace_with: replace_with,
+    #   line: line
+    # }
   end
 end
 
