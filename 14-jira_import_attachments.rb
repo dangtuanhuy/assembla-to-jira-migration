1a48e5099442ae51d759573b897d141a91f30a6f
Geoff Douglas <gdoug@guidance.com>
2018-01-30 19:44:23 -0700

    Update the JIRA Attachments to fail + continue.

--- 14-jira_import_attachments.rb
+++ 14-jira_import_attachments.rb
@@ -18,8 +18,6 @@ tickets_jira_csv = "#{OUTPUT_DIR_JIRA}/jira-tickets.csv"
 
 @total_tickets = @tickets_jira.length
 
-goodbye("Invalid arg='#{ARGV[0]}', cannot be greater than the number of tickets=#{@total_tickets}") if restart_offset > @total_tickets
-
 # TODO: Move this to ./lib/tickets-assembla.rb and reuse in other scripts.
 @a_id_to_a_nr = {}
 @a_id_to_j_id = {}
@@ -36,6 +34,7 @@ downloaded_attachments_csv = "#{OUTPUT_DIR_JIRA}/jira-attachments-download.csv"
 @attachments_total = @downloaded_attachments.length
 
 puts "Total attachments: #{@attachments_total}"
+goodbye("Invalid arg='#{ARGV[0]}', cannot be greater than the number of attachments=#{@attachments_total}") if restart_offset > @attachments_total
 
 # IMPORTANT: Make sure that the downloads are ordered chronologically from first (oldest) to last (newest)
 @downloaded_attachments.sort! { |x, y| x['created_at'] <=> y['created_at'] }
@@ -69,14 +68,15 @@ puts "Total attachments: #{@attachments_total}"
   counter = index + 1
   next if counter < restart_offset
   percentage = ((counter * 100) / @attachments_total).round.to_s.rjust(3)
-  payload = { mulitpart: true, file: File.new(filepath, 'rb') }
-  base64_encoded = if JIRA_SERVER_TYPE == 'hosted'
-                     Base64.encode64(created_by + ':' + created_by)
-                   else
-                     Base64.encode64(JIRA_API_ADMIN_EMAIL + ':' + ENV['JIRA_API_ADMIN_PASSWORD'])
-                   end
-  headers = { 'Authorization': "Basic #{base64_encoded}", 'X-Atlassian-Token': 'no-check' }
   begin
+    payload = { mulitpart: true, file: File.new(filepath, 'rb') }
+    base64_encoded = if JIRA_SERVER_TYPE == 'hosted'
+                       Base64.encode64(created_by + ':' + created_by)
+                     else
+                       Base64.encode64(JIRA_API_ADMIN_EMAIL + ':' + ENV['JIRA_API_ADMIN_PASSWORD'])
+                     end
+    headers = { 'Authorization': "Basic #{base64_encoded}", 'X-Atlassian-Token': 'no-check' }
+  
     response = RestClient::Request.execute(method: :post, url: url, payload: payload, headers: headers)
     puts "#{percentage}% [#{counter}|#{@attachments_total}] POST #{url} '#{filename}' (#{content_type}) => OK"
     result = JSON.parse(response.body)
