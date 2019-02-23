d162b4f269481a5f07eb3e704b8d59c1942b1bfc
Kiffin Gish <kiffin.gish@planet.nl>
2019-02-23 20:49:45 +0100

    Fixed incorrect parent child calculations.

--- wiki_to_confluence.rb
+++ wiki_to_confluence.rb
@@ -2,68 +2,82 @@
 
 load './lib/common.rb'
 
+def abort(message)
+  puts "ERROR: #{message} => EXIT"
+  exit
+end
+
 # id,page_name,contents,status,version,position,wiki_format,change_comment,parent_id,space_id,user_id,created_at,updated_at
 wiki_assembla_csv = "#{OUTPUT_DIR_ASSEMBLA}/wiki-pages.csv"
 @wiki_assembla = csv_to_array(wiki_assembla_csv)
 
-pages = {}
+@pages = {}
+
+puts "\n--- Wiki pages: #{@wiki_assembla.length} ---\n"
 
 @wiki_assembla.each do |wiki|
   id = wiki['id']
-  if pages[id]
-    puts "Detected duplicate id='#{id}' => SKIP"
-  else
-    pages[id] = {
-        page: wiki,
-        parent: @wiki_assembla.find { |page| page['id'] === wiki['parent_id'] },
-        children: []
-    }
-  end
+  page_name = wiki['page_name']
+  abort "Duplicate id='#{id}'" if @pages[id]
+  abort "Duplicate page_name='#{page_name}'" if @pages.detect { |_, value| value[:page]['page_name'] == page_name }
+  @pages[id] = { page: wiki, children_ids: [] }
 end
 
-pages.each do |id, value|
-  parent = value[:parent]
-  next unless parent
-  parent_id = parent['id']
-  page = pages[parent_id]
-  if page
-    page[:children] << pages[id]
-  else
-    puts "Cannot find page with parent_id='#{parent_id}'"
-  end
+def show_page(id, offset = [])
+  # puts "show_page() id='#{id}' offset=[#{offset.join(',')}]"
+  pages_id = @pages[id]
+  page = pages_id[:page]
+  page_name = page['page_name']
+  parent_id = page['parent_id'] ? "parent_id='#{page['parent_id']}'" : ''
+  created_at = page['created_at'].sub(/\.[^.]*$/, '')
+  children_ids = pages_id[:children_ids]
+  children_ids = 'children_ids=' + (children_ids.length.positive? ? "#{children_ids.length} [#{children_ids.join(',')}]" : '0')
+
+  tree = offset.join('-')
+  tree += ' ' if tree.length.positive?
+  puts "#{tree}id='#{id}' #{parent_id}created_at='#{created_at}' page_name='#{page_name}' #{children_ids}"
 end
 
-puts '---'
+def show_page_tree(id, offset)
+  # puts "show_page_tree() id='#{id}' offset=[#{offset.join(',')}]"
+  show_page(id, offset)
+  pages_id = @pages[id]
+  children_ids = pages_id[:children_ids]
+  return unless children_ids.length.positive?
 
-pages.each do |id, value|
-  parent = value[:parent]
-  next if parent
-  children = pages[id][:children]
-  count = children.length
-  if count.positive?
-    children_ids = children.map { |child| child[:page]['id']}
-    puts "id='#{id}' children='#{count}' ids='#{children_ids.join(',')}'"
-  else
-    puts "id='#{id}' no children"
-  end
+  # Child pages sorted by created_at
+  children_ids.sort_by! { |child_id| @pages[child_id][:page]['created_at'] }
+  children_ids.each_with_index { |child_id, index| show_page_tree(child_id, offset.dup << index) }
 end
 
-puts '---'
+@pages.each do |id, value|
+  parent_id = value[:page]['parent_id']
+  next unless parent_id
 
-pages.each do |id, value|
-  parent = value[:parent]
-  next unless parent
-  parent_id = parent['id']
-  children = pages[id][:children]
-  count = children.length
-  if count.positive?
-    children_ids = children.map { |child| child[:page]['id']}
-    puts "id='#{id}' parent_id='#{parent_id}' children='#{count}' ids='#{children_ids.join(',')}'"
-  else
-    puts "id='#{id}' parent_id='#{parent_id}' no children"
-  end
+  parent = @pages[parent_id]
+  abort "Cannot find parent page with parent_id='#{parent_id}'" unless parent
+  parent[:children_ids] << id
 end
 
-puts '---'
+# Parent pages sorted by created_at
+@parent_pages = @pages.reject { |_, value| value[:page]['parent_id'] }.sort_by { |_, value| value[:page]['created_at'] }
+puts "\n--- Parents: #{@parent_pages.length} ---\n"
+@parent_pages.each { |id, _| show_page(id) }
 
-puts "Done!"
+puts "\nDone\n"
+
+# Child pages sorted by created_at
+@child_pages = @pages.select { |_, value| value[:page]['parent_id'] }.sort_by { |_, value| value[:page]['created_at'] }
+puts "\n--- Children: #{@child_pages.length} ---\n"
+@child_pages.each { |id, _| show_page(id) }
+puts "\nDone\n"
+
+puts "\n--- Page Tree: #{@pages.length} ---\n"
+
+count = 0
+@parent_pages.each do |id, _|
+  show_page_tree(id, [count])
+  count += 1
+end
+
+puts "\nDone\n"
