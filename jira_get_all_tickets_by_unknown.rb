c652c6ed867bedf58c4bd1758879d496098e4cf0
Kiffin Gish <kiffin.gish@planet.nl>
2019-12-14 12:02:56 +0100

    Script to list all tickets linked to unknown users.

--- jira_get_all_tickets_by_unknown.rb
+++ jira_get_all_tickets_by_unknown.rb
@@ -9,6 +9,11 @@ load './lib/common.rb'
 tickets_jira_csv = "#{OUTPUT_DIR_JIRA}/jira-tickets.csv"
 @tickets_jira = csv_to_array(tickets_jira_csv).select { |ticket| ticket['result'] == 'OK' }
 
+# --- JIRA comments --- #
+# jira_comment_id,jira_ticket_id,jira_ticket_key,assembla_comment_id,assembla_ticket_id,user_login,body
+comments_jira_csv = "#{OUTPUT_DIR_JIRA}/jira-comments.csv"
+@comments_jira = csv_to_array(comments_jira_csv)
+
 @names = []
 @unknown_users = {}
 @tickets_jira.select { |t| t['reporter_name'] =~ /unknown/i }.each do |t|
@@ -16,15 +21,50 @@ tickets_jira_csv = "#{OUTPUT_DIR_JIRA}/jira-tickets.csv"
   key = t['jira_ticket_key']
   unless @unknown_users[name]
     @names << name
-    @unknown_users[name] = []
+    @unknown_users[name] = {}
+    @unknown_users[name]['description'] = []
   end
-  @unknown_users[name] << key
+  @unknown_users[name]['description'] << key
 end
 
-@names.sort_by{|n| n.split('-')[1].to_i}.each do |name|
-  keys = @unknown_users[name]
+@comments_jira.select { |c| c['user_login'] =~ /unknown/i}.each do |c|
+  next unless c['body'].nil? || c['body'].length.zero?
+  name = c['user_login']
+  id = c['jira_comment_id']
+  key = c['jira_ticket_key']
+  unless @unknown_users[name]
+    @names << name
+    @unknown_users[name] = {}
+    @unknown_users[name]['comments'] = []
+  end
+  unless @unknown_users[name]['comments']
+    @unknown_users[name]['comments'] = []
+  end
+  @unknown_users[name]['comments'] << key
+end
+
+puts
+puts '--- DESCRIPTIONS ---'
+puts
+
+@names.sort_by { |n| n.split('-')[1].to_i }.each do |name|
+  keys = @unknown_users[name]['description']
+  next unless keys
   puts "#{name}: #{keys.length}"
   keys.each do |k|
     puts "* #{k}"
   end
 end
+
+#puts
+#puts '--- COMMENTS ---'
+#puts
+#
+#@names.sort_by { |n| n.split('-')[1].to_i }.each do |name|
+#  keys = @unknown_users[name]['comments']
+#  next unless keys
+#  puts "#{name}: #{keys.length}"
+#  keys.each do |k|
+#    puts "* #{k}"
+#  end
+#end
